237c4de080d5f8b4828f9bc7099e2887953cc313
Alexander Grygoriev <truedevelop@yahoo.com>
2024-08-18 14:10:30 +0300

    before starting internationalization

---
+++ src/pages/signIn/index.tsx
@@ -0,0 +1,15 @@
+// import LoginForm from '@/shared/components/LoginForm/LoginForm'
+// import { HeadMeta } from '@/shared/components/headMeta/HeadMeta'
+// import { useRouterLocaleDefinition } from '@/shared/hooks/useRouterLocaleDefinition'
+
+export default function SignIn() {
+  // const routerLocale = useRouterLocaleDefinition()
+
+  return (
+    <>
+      {/* <HeadMeta title={routerLocale.signInPage.title} /> */}
+      {/* <LoginForm /> */}
+      <div>Sign IN page</div>
+    </>
+  )
+}
