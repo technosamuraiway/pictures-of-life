c07d55a42573b844fabfdf9c9300c528a983433e
max1k1 <63220372+max1k1@users.noreply.github.com>
2024-08-20 13:35:50 +0300

    add: Layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,14 @@ import type { AppProps } from 'next/app'
 import '@/styles/globals.scss'
 import '@/styles/_colors.scss'
 import '@/styles/_typography.scss'
+import Layout from '@/shared/components/Layout/Layout'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </main>
   )
 }
