3c96ecc54edb410134313ae58dceb4ca0ad83fdd
nadzeya <vitserresvit@gmail.com>
2024-08-27 14:46:44 +0300

    feat: added logic for header

--- src/shared/components/Layout/Layout.tsx
+++ src/shared/components/Layout/Layout.tsx
@@ -1,7 +1,9 @@
 import React, { ReactNode } from 'react'
 
+import { useRouterLocaleDefinition } from '@/shared/hooks/useRouterLocaleDefinition'
 import { Header } from '@technosamurai/techno-ui-kit'
 import { NextPage } from 'next'
+import { useRouter } from 'next/router'
 
 import s from '@/shared/components/Layout/Layout.module.scss'
 
@@ -10,9 +12,21 @@ type Props = {
 }
 
 const Layout: NextPage<Props> = ({ children }) => {
+  const router = useRouter()
+  const t = useRouterLocaleDefinition()
+  const changeLangHandler = (locale: string) => {
+    const { asPath, pathname, query } = router
+
+    router.push({ pathname, query }, asPath, { locale })
+  }
+
+  const handleLogoClick = () => {
+    router.push('/')
+  }
+
   return (
     <div className={s.layout}>
-      <Header />
+      <Header changeLangHandler={changeLangHandler} onLogoClick={handleLogoClick} />
       <main>{children}</main>
     </div>
   )
