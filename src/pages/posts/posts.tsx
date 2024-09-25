05bf943b6514147f122ae1cfa37ee9da46da887c
Alexander Grygoriev <truedevelop@yahoo.com>
2024-09-25 08:17:58 +0300

    base experiments with public posts

--- src/pages/posts/posts.tsx
+++ src/pages/posts/posts.tsx
@@ -3,12 +3,17 @@ import * as React from 'react'
 
 import { useGetAllPublicPostsQuery } from '@/services/flow/post.service'
 import { ButtonLink, PATH, useRouterLocaleDefinition } from '@/shared'
+import { convertDate } from '@/shared/utils/convertData'
 
 type Props = {}
 
 export default function Posts(props: Props) {
   const t = useRouterLocaleDefinition()
-  const { data: allPosts } = useGetAllPublicPostsQuery()
+  const { data: allPublicPosts, isError, isLoading } = useGetAllPublicPostsQuery()
+
+  if(isLoading) return <div>Loading...</div>
+  if(isError) return <div style={{color: 'red'}}>Something Wrong...</div>
+
 
   return (
     <div
@@ -22,7 +27,23 @@ export default function Posts(props: Props) {
       }}
     >
       <ButtonLink linkHref={PATH.HOME} title={t.error404Page.btnText} variant={'secondary'} />
-      <div>Posts</div>
+
+      <div style={{
+        alignItems: 'center',
+        display: 'flex',
+        // flexDirection: 'column',
+        gap: '50px',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+        margin: '30px auto',
+      }}>{allPublicPosts?.items?.map(post => (
+        <div key={post.id}>
+          <p>{post.owner.firstName} {post.owner.lastName}</p>
+          <p>{convertDate(post.createdAt)} {post.userName}</p>
+            <img width={300} height={300} src={post.images[0].url} alt='img'/>
+        </div>
+      ))}
+      </div>
     </div>
   )
 }
--- src/services/flow/post.service.ts
+++ src/services/flow/post.service.ts
@@ -1,9 +1,10 @@
+import { IPostPublicResponse } from '@/pages/posts/typesPost'
 import { inctagramApi } from '../api/inctagram.api'
 
 export const postService = inctagramApi.injectEndpoints({
   endpoints: builder => {
     return {
-      getAllPublicPosts: builder.query<void, void>({
+      getAllPublicPosts: builder.query<IPostPublicResponse, void>({
         query: () => {
           return { url: `v1/public-posts/all` }
         },
