22ae9f1cf31ec177ae938ef6016e8039c6a02ae4
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-09-26 08:01:23 +0300

    TEC-38 UC-1 feat: new modal

--- src/entities/modals/createNewPostModal/editPostPhotoModal/EditPostPhotoModal.tsx
+++ src/entities/modals/createNewPostModal/editPostPhotoModal/EditPostPhotoModal.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { toast } from 'react-toastify'
 
 import { ActionConfirmationModal } from '@/entities'
+import { PostWithoutHeaderModal } from '@/entities/modals/postWithoutHeaderModal/PostWithoutHeaderModal'
 import { PATH, useRouterLocaleDefinition } from '@/shared'
 import { Modal } from '@technosamurai/techno-ui-kit'
 import { useRouter } from 'next/router'
@@ -16,46 +17,14 @@ interface IProps {
 
 export const EditPostPhotoModal = ({ image, onOpen, setOnOpen }: IProps) => {
   const t = useRouterLocaleDefinition()
-  const { push } = useRouter()
-  const [openExitModal, setOpenExitModal] = useState<boolean>(false)
-
-  const onCloseModalHandler = () => {
-    setOpenExitModal(true)
-  }
-
-  const onModalDiscardDtnClickHandler = () => {
-    setOnOpen(false)
-    push(PATH.HOME)
-  }
-
-  const onDraftBtnClickHandler = () => {
-    setOnOpen(false)
-    toast.info('Здесь будет функционал черновиков, когда-нибудь точно появится!')
-    push(PATH.HOME)
-  }
 
   return (
-    <>
-      <Modal
-        contentClassName={s.wrapper}
-        headerTitle={t.createNewPost.editPhotoModal.modalTitle}
-        modalSize={'M'}
-        onOpenChange={onCloseModalHandler}
-        open={onOpen}
-      >
-        Hello
-      </Modal>
-      <ActionConfirmationModal
-        buttonsWrapperCN={s.modalButtons}
-        headerTitle={t.createNewPost.editPhotoModal.modalExitTitle}
-        isOpenModal={openExitModal}
-        modalTextChildren={t.createNewPost.editPhotoModal.modalExitText}
-        negativeButtonChildren={t.createNewPost.editPhotoModal.modalExitSaveDraftBtn}
-        onClickNegativeButton={onDraftBtnClickHandler}
-        onClickPositiveButton={onModalDiscardDtnClickHandler}
-        positiveButtonChildren={t.createNewPost.editPhotoModal.modalExitDiscardBtn}
-        setIsOpenModal={setOpenExitModal}
-      />
-    </>
+    <PostWithoutHeaderModal
+      headerTitle={t.createNewPost.editPhotoModal.modalTitle}
+      onOpen={onOpen}
+      setOnOpen={setOnOpen}
+    >
+      Hello
+    </PostWithoutHeaderModal>
   )
 }
