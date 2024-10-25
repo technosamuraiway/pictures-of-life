f4b5f162887ce8befd3a3359ba7e0a729ae4f84c
Mikhail Kuznetsov <mk.mikhail.k@gmail.com>
2024-10-25 18:12:11 +0300

    => ver.11

--- src/shared/hooks/index.ts
+++ src/shared/hooks/index.ts
@@ -1,4 +1,3 @@
 export { useGoogleAuth } from './auth/useGoogleAuth'
 export { useLogout } from './auth/useLogout'
-export { useMeWithRouter } from './meWithRouter/useMeWithRouter'
 export { useRouterLocaleDefinition } from './useRouterLocaleDefinition'
