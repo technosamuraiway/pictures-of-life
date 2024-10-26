0eae2d24889e65ec530481da5acaa17056d7e460
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-10-26 16:57:05 +0300

    TEC-54 UC-3 feat: add SSR for public post by id

--- src/pages/start/[userId].tsx
+++ src/pages/start/[userId].tsx
@@ -1,4 +1,4 @@
-import { AvatarsType, IPostPublicResponse } from '@/services'
+import { AvatarsType, IPostPublicResponse, IPostUser } from '@/services'
 import { getBaseLayout } from '@/widgets'
 import { Typography } from '@technosamurai/techno-ui-kit'
 import { GetServerSideProps } from 'next'
@@ -14,25 +14,28 @@ interface IResponse {
 }
 
 interface IProps {
+  post: IPostUser
   posts: IPostPublicResponse
   user: IResponse
 }
 
 export const getServerSideProps: GetServerSideProps<IProps> = async ({ query }) => {
-  const { userId } = query
+  const { postId, userId } = query
 
-  const [userResponse, postsResponse] = await Promise.all<Response>([
+  const [userResponse, postsResponse, postResponse] = await Promise.all<Response>([
     fetch(`https://inctagram.work/api/v1/public-user/profile/${userId}`),
     fetch(`https://inctagram.work/api/v1/public-posts/user/${userId}`),
+    fetch(`https://inctagram.work/api/v1/public-posts/${postId}`),
   ])
 
+  const post: IPostUser = await postResponse.json()
   const posts: IPostPublicResponse = await postsResponse.json()
   const user: IResponse = await userResponse.json()
 
-  return { props: { posts, user } }
+  return { props: { post, posts, user } }
 }
 
-const StartSSR = ({ posts, user }: IProps) => {
+const StartSSR = ({ post, posts, user }: IProps) => {
   return (
     <div>
       <div className={s.wrapperUser}>
@@ -45,11 +48,18 @@ const StartSSR = ({ posts, user }: IProps) => {
         <Typography variant={'bold-text-16'}>{user?.userName}:</Typography>
         <Typography variant={'bold-text-14'}>{user?.aboutMe}</Typography>
       </div>
+
+      <div className={s.wrapperUser}>
+        <Image alt={`${post?.userName} - avatar`} height={200} src={post.avatarOwner} width={200} />
+        <Typography variant={'bold-text-16'}>{post?.userName}:</Typography>
+        <Typography variant={'bold-text-14'}>{post?.description}</Typography>
+      </div>
+
       <div className={s.wrapperPosts}>
         <Typography className={s.text} variant={'regular-text-14'}>
           Все мои посты : {posts.items.length}{' '}
         </Typography>
-        {posts?.items.map((el, i) => {
+        {posts?.items.map(el => {
           return (
             <Image
               alt={'posts image'}
