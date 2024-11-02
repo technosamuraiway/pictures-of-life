80954aee908d11d49459ab195d637371d24d9055
max1k1 <63220372+max1k1@users.noreply.github.com>
2024-11-02 22:43:58 +0200

    UC-1, TEC-60, fix: merge fix 2

--- src/widgets/index.ts
+++ src/widgets/index.ts
@@ -1,9 +1,11 @@
 // ============================== [ auth ] ======================================
 export { NewPassword } from './auth/newPassword/NewPassword'
 export { ResendLink } from './auth/resendLink/ResendLink'
+
 // ============================== [ layouts ] ======================================
 export { getBaseLayout } from './layouts/BaseLayout'
 export { getLayoutWithNav } from './layouts/LayoutWithNav'
+
 // ============================== [ profile ] ======================================
 export { GeneralInfo } from './profile/settings/generalInfo/GeneralInfo'
 export { Management } from './profile/management/Management'
