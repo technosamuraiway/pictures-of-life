c162101734d7362220a591adef7a0fa108db06ad
max1k1 <63220372+max1k1@users.noreply.github.com>
2024-11-02 22:41:31 +0200

    UC-1, TEC-60, fix: merge fix

--- src/widgets/index.ts
+++ src/widgets/index.ts
@@ -1,16 +1,15 @@
 // ============================== [ auth ] ======================================
 export { NewPassword } from './auth/newPassword/NewPassword'
 export { ResendLink } from './auth/resendLink/ResendLink'
-
 // ============================== [ layouts ] ======================================
 export { getBaseLayout } from './layouts/BaseLayout'
-
 export { getLayoutWithNav } from './layouts/LayoutWithNav'
-
-export { InfoPanel } from './profile/posts/infoPanel/InfoPanel'
-export { PostsShower } from './profile/posts/postsShower/PostsShower'
-
 // ============================== [ profile ] ======================================
 export { GeneralInfo } from './profile/settings/generalInfo/GeneralInfo'
 export { Management } from './profile/management/Management'
 export { Devices } from './profile/settings/devices/Devices'
+export { useProfilePage } from './profile/lib/hooks/useProfilePage'
+export { InfoPanel } from './profile/posts/infoPanel/InfoPanel'
+export { PostsShower } from './profile/posts/postsShower/PostsShower'
+export { ProfilePostModal } from './profile/profilePostModal/ProfilePostModal'
+export type { PostWithId, PostsAssociativeArray } from './profile/lib/types/profileTypes'
